85614af7b2788261e5caacfcb53799a5e194cb40
Arun-Tiwary <70338393+Arun-Tiwary@users.noreply.github.com>
2021-03-16 18:58:42 +0530

    feat:list-of-work

--- src/components/listtodo.js
+++ src/components/listtodo.js
@@ -1,3 +1,28 @@
-export const ListToDo()=>{
-  
-}
+const ListToDo = ({ todos, setTodos }) => {
+  // const toggleHAndler = (id) => {
+  //   setTodos((todos) =>
+  //     todos.map((todo) => {
+  //       if (todo.id === id) {
+  //         todo.completed = !todo.completed;
+  //       }
+  //       return todo;
+  //     })
+  //   );
+  // };
+  return (
+    <div>
+      {todos.map(({ id, title, completed }) => (
+        <li
+          // onClick={toggleHAndler(id)}
+          // style={completed ? {backgroundColor: "red"} : null}
+          style={{ textAlign: "left", marginLeft: "200px" }}
+        >
+          {title}
+        </li>
+      ))}
+      <h3>Total={todos.length}</h3>
+    </div>
+  );
+};
+
+export default ListToDo;
